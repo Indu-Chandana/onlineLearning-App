6fd83b2ebdeb0474a4539d8d64627fc829c3ec6e
Indu-Chandana <indu.99.ic@gmail.com>
2023-12-22 23:09:18 +0530

    Dec22 001

--- screens/Course/CourseDetails.js
+++ screens/Course/CourseDetails.js
@@ -235,6 +235,7 @@ const CourseDetails = ({ navigation, route }) => {
                             >
                                 {/* Hello */}
                                 {/* Hello */}
+                                {/* Hello */}
                             </View>
                         )
                     }}
